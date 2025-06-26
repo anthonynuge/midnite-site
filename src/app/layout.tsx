af1c0a4014b19a5aaea4b1aa1f74a35225ace65c
antho <anthonynuge0509@gmail.com>
2025-06-26 17:41:27 -0500

    Scaffold out main layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,36 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "@/styles/globals.css";
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import '@/styles/globals.css'
+import Navbar from '@/components/layout/Navbar'
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
-  title: "Midnite Marketing Agency",
-  description: "Marketing that never sleeps",
-};
+  title: 'Midnite Marketing Agency',
+  description: 'Marketing that never sleeps',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} flex flex-col items-center justify-center antialiased`}
       >
+        <Navbar />
         {children}
       </body>
     </html>
-  );
+  )
 }
