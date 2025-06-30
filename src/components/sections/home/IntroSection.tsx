d16a860a73b933f551c6b72c2763ed498eac44c6
antho <anthonynuge0509@gmail.com>
2025-06-30 18:43:55 -0500

    Make section mobile responsive.

--- src/components/sections/home/IntroSection.tsx
+++ src/components/sections/home/IntroSection.tsx
@@ -4,15 +4,16 @@ export default function IntroSection() {
   return (
     <div className="grid grid-cols-1 place-items-center items-center gap-2 sm:grid-cols-2">
       {/* Left section */}
-      <div className="flex max-w-3/4 flex-col gap-4">
-        <h2 className="text-4xl font-bold">
-          The Creative Force Behind <br />
+      <div className="flex max-w-full flex-col gap-4 md:max-w-3/4">
+        <h2 className="text-2xl font-bold md:text-4xl">
+          The Creative Force Behind
+          <br className="hidden xl:block" />
           Your Brand Success
         </h2>
         <p className="text-muted-foreground">
           We are a results-driven marketing agency dedicated to helping brands unlock their full
           potential and achieve sustainable revenue growth. We specialize in crafting tailored
-          strategies that amplify your brand&pos;s presence, engage your target audience, and
+          strategies that amplify your brand&apos;s presence, engage your target audience, and
           deliver measurable results. At Midnite Agency, we partner with you to illuminate your path
           to success, combining creativity, data, and innovation to drive your business forward.
         </p>
