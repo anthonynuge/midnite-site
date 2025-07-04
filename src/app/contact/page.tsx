b2842337daa98997d8bda2bec84ee1c9cc005b7d
antho <anthonynuge0509@gmail.com>
2025-07-03 22:43:13 -0500

    Clean up and animate in conctact page.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,52 +1,50 @@
 import ContactForm from '@/components/forms/ContactForm'
+import ContactSection from '@/components/sections/contact/ContactSection'
 import ResponsiveWrapper from '@/components/layout/ResponsiveWrapper'
 import { IconLink } from '@/components/ui/IconLink'
 import { FaFacebook, FaInstagram } from 'react-icons/fa6'
+import FadeInSection from '@/components/animations/FadeInSection'
 
 export default function ContactPage() {
   return (
     <>
-      {/* <main className="w-full max-w-screen-2xl bg-amber-300 pt-24">
-        <section className="bg-amber-700"> */}
       <ResponsiveWrapper fullHeight className="max-w-screen-2xl">
-        <div className="grid grid-cols-1 gap-4 py-6 pt-24 md:grid-cols-2 md:pt-42">
-          <div className="flex flex-col place-content-center space-y-2 lg:max-w-3/4">
-            <h2 className="font-serif text-4xl font-bold md:text-6xl">Contact Us</h2>
-            <p className="text-sm md:text-base">
-              We&apos;re here to help you with any questions or concerns you may have. Please fill
-              out the form below and we&apos;ll get back to you as soon as possible.
-            </p>
+        <FadeInSection>
+          <div className="grid grid-cols-1 gap-4 py-6 pt-24 md:grid-cols-2 md:pt-42">
+            <div className="flex flex-col place-content-center space-y-2 lg:max-w-3/4">
+              <ContactSection />
 
-            {/* Contact Info */}
-            <div className="flex flex-col space-y-2">
-              <div className="text-base font-semibold md:text-lg">midenit-agency@gmail.com</div>
-              <div className="text-base font-semibold md:text-lg">+49 176 777 777 77</div>
+              {/* Contact Info */}
+              <div className="flex flex-col space-y-2">
+                <div className="text-base font-semibold md:text-lg">
+                  outreach@midnite-agency.com
+                </div>
+                <div className="text-base font-semibold md:text-lg">+49 176 777 777 77</div>
 
-              {/* Social Links */}
-              <div className="flex gap-4">
-                <IconLink
-                  href={'https://www.instagram.com/midenit_agency/'}
-                  icon={FaInstagram}
-                  newTab
-                  size={25}
-                />
-                <IconLink
-                  href={'https://www.facebook.com/midenit_agency/'}
-                  icon={FaFacebook}
-                  newTab
-                  size={25}
-                />
+                {/* Social Links */}
+                <div className="flex gap-4">
+                  <IconLink
+                    href={'https://www.instagram.com/midnite_agency/'}
+                    icon={FaInstagram}
+                    newTab
+                    size={25}
+                  />
+                  <IconLink
+                    href={'https://www.facebook.com/people/Midnite-Agency/61557421103969/?_rdr'}
+                    icon={FaFacebook}
+                    newTab
+                    size={25}
+                  />
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="bg-card/75 rounded-lg px-6 py-8 backdrop-blur-lg">
-            <ContactForm />
+            <div className="bg-card/75 rounded-lg px-6 py-8 backdrop-blur-lg">
+              <ContactForm />
+            </div>
           </div>
-        </div>
+        </FadeInSection>
       </ResponsiveWrapper>
-      {/* </section>
-      </main> */}
     </>
   )
 }
